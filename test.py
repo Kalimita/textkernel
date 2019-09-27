215a01900747beac436f72c7716770cf16b8bb28
Gülsen Konuk <48500112+Kalimita@users.noreply.github.com>
2019-09-27 15:43:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 headers = {'User-Agent': 'Mozilla/5.0'}
-payload = {'account':'tkdemo_ml_cv', 'username': 'behrud_demo', 'password':'.8sT0%Cjc'}
+payload = {'account':'account_name', 'username': 'user_name', 'password':'password'}
 files = {'upload_file': open('cv.docx','rb')}
 
 session = requests.Session()
