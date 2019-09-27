dce584a41e3c4901aa3e98d9b5549d1affa5b3bb
Gülsen Konuk <48500112+Kalimita@users.noreply.github.com>
2019-09-27 15:49:39 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-headers = {'User-Agent': 'Mozilla/5.0'}
-payload = {'account':'account_name', 'username': 'user_name', 'password':'password'}
-files = {'upload_file': open('cv.docx','rb')}
-
-session = requests.Session()
-r = session.post('https://home.textkernel.nl/match/extract.do?useJsonErrorMsg=true',headers=headers,files=files,data=payload)
-
-print(r.body)
